e8979d92ada81e37566f10405ddfc2ce8cdd4737
Joseph Nix <nixjdm@terminallabs.com>
2018-04-16 14:05:46 -0500

    [master] Refactoring to process on build, this avoids a 'double processing' that prevented the easy use of {% raw %}.

--- lektor_jinja_content.py
+++ lektor_jinja_content.py
@@ -1,36 +1,37 @@
 # -*- coding: utf-8 -*-
-import click
 from jinja2.exceptions import TemplateSyntaxError, UndefinedError
 
+from lektor.context import get_ctx
+from lektor.db import Page
 from lektor.pluginsystem import Plugin
 
 class JinjaContentPlugin(Plugin):
     name = 'lektor-jinja-content'
     description = u'Render content fields with Jinja2.'
 
-    def jinjaify(self, context, data, field):
-        '''Process each content field as if it's Jinja.'''
+    def jinjaify(self, pad, source, data, field):
+        '''Process each content field as if it's Jinja with the normal Template Context.'''
+        context = {}
+        context['site'] = pad
+        context['this'] = source
+        context['alt'] = source.alt
+        context['config'] = pad.config
         try:
             data = self.env.jinja_env.from_string(data).render(context)
         except TypeError: # Content is not a string or template. Skip it.
             pass
-        except (TemplateSyntaxError, UndefinedError):
-            click.secho("Warning, you have a Jinja2 error for "
-                        "{} content field '{}'.\n"
-                        "You likely didn't expect this content to be rendered by Jinja.\n"
-                        "Fix this by making it valid Jinja, "
-                        "escaping that section (e.g. with {{% raw %}}),\n"
-                        "or by disabling this plugin {} for this section."
-                        .format(context['this'].path, field, self.name), fg='red')
+        except TemplateSyntaxError:
             raise
         return data
 
-    def on_process_template_context(self, context, **extra):
-        for field in context['this']._data.keys():
-            data = context['this']._data.get(field)
+    def on_before_build(self, builder, build_state, source, prog):
+        pad = builder.pad
+        if isinstance(prog.source, Page): # skips non-page Records and Assets
+            for field in prog.source._data.keys():
+                data = prog.source._data.get(field)
+                if data:
+                    try: # E.g. for fields like Markdown. Not all types have `source`.
+                        prog.source._data[field].source = self.jinjaify(pad, prog.source, data.source, field)
+                    except AttributeError:
+                        prog.source._data[field] = self.jinjaify(pad, prog.source, data, field)
 
-            if data:
-                try: # E.g. for fields like Markdown. Not all types have `source`.
-                    context['this']._data[field].source = self.jinjaify(context, data.source, field)
-                except AttributeError:
-                    context['this']._data[field] = self.jinjaify(context, data, field)
