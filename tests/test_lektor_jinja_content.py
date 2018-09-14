cb03bb1d385907fdf84404c0099ceef1e2c7d23d
Joseph Nix <nixjdm@terminallabs.com>
2018-09-14 15:51:44 -0500

    [test-plugins] Adding test for lektor-tags compatibility.

--- tests/test_lektor_jinja_content.py
+++ tests/test_lektor_jinja_content.py
@@ -1,6 +1,6 @@
 import os
 
-test_positive_strings = [
+root_positive_strings = [
     "<p>In Jinja!",
     "<p>this.path = /</p>",
     "page_num",
@@ -15,17 +15,29 @@ test_positive_strings = [
     "This is in a nested flowblock.",
 ]
 
-test_negative_strings = ["False!", "{% raw %}", "{% endraw %}", "Not visible!", "{{"]
+root_negative_strings = ["False!", "{% raw %}", "{% endraw %}", "Not visible!", "{{"]
 
 
 def test_builder(pad, builder):
     failures = builder.build_all()
     assert not failures
-    page_path = os.path.join(builder.destination_path, "index.html")
-    html = open(page_path).read()
+    root_path = os.path.join(builder.destination_path, "index.html")
+    with open(root_path) as root_file:
+        root_html = root_file.read()
 
-    for string in test_positive_strings:
-        assert string in html
+    for string in root_positive_strings:
+        assert string in root_html
 
-    for string in test_negative_strings:
-        assert string not in html
+    for string in root_negative_strings:
+        assert string not in root_html
+
+    # lektor-tags compatibility
+    tag_path = os.path.join(builder.destination_path, "tag/index.html")
+    with open(tag_path) as tag_file:
+        tag_html = tag_file.read()
+    subpage_path = os.path.join(builder.destination_path, "subpage/index.html")
+    with open(subpage_path) as subpage_file:
+        subpage_html = subpage_file.read()
+
+    assert '<li><a href="../subpage/">subpage</a></li>' in tag_html
+    assert '<a href="../tag">' in subpage_html
